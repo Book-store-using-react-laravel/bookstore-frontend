9d52313b900980bba559909c13aad8b3be815e73
Harsh0726 <harshanineranjana@gmail.com>
2023-11-04 00:33:22 +0530

    form required fields

--- src/components/bookForm.js
+++ src/components/bookForm.js
@@ -15,6 +15,7 @@ function BookForm({
     setImageFiles,
     handleSubmit,
 }) {
+
     return (
         <div>
             <h4>Create a New Book</h4>
@@ -22,23 +23,23 @@ function BookForm({
             <Form className='book-form' onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formGroupText">
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                    <Form.Control required type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupText">
                     <Form.Label>Author</Form.Label>
-                    <Form.Control type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <Form.Control required type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupPrice">
                     <Form.Label>Price</Form.Label>
-                    <Form.Control type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+                    <Form.Control required type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupNumber">
                     <Form.Label>Stock</Form.Label>
-                    <Form.Control type="number" value={stock} onChange={(e) => setStock(e.target.value)} />
+                    <Form.Control required type="number" value={stock} onChange={(e) => setStock(e.target.value)} placeholder='0'/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formGroupImage">
                     <Form.Label>Images</Form.Label>
-                    <Form.Control type="file" multiple onChange={(e) => setImageFiles(e.target.files)} />
+                    <Form.Control required type="file" multiple onChange={(e) => setImageFiles(e.target.files)} />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
