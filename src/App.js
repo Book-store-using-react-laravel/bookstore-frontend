376da8220cdc4c9fd4651b459ab03fcb2524d718
Harsh0726 <harshanineranjana@gmail.com>
2023-11-04 18:45:32 +0530

    Added NextUI dependencies an tailwind

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import BookList from './components/BookList';
+// import BookList from './components/BookList';
 import LeftTabs from './components/LeftTabs';
-
+import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
   return (
-    <div className='cont'>
-      <div className='bottom'><h3>BookStore</h3></div>
-      <div className='body'><LeftTabs/></div>
-      <div className='footer'><p>footer</p></div>
-      {/* <BookList/> */}
-    </div>
+    <NextUIProvider>
+      <div className='cont'>
+        <div className='bottom'><h3>BookStore</h3></div>
+        <div className='body'><LeftTabs/></div>
+        <div className='footer'><p>footer</p></div>
+        {/* <BookList/> */}
+      </div>
+    </NextUIProvider>
   );
 }
 
